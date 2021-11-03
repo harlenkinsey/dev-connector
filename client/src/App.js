36c5e5cbd9a0fe76f8571e867e5bcffd45bcaab1
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-11-03 13:32:29 -0500

    added Alert component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -18,8 +19,11 @@ const App = () => (
         <Navbar />
         <Route exact path ='/' component={Landing} />
         <section className='container'>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
         </section>
       </Fragment>
     </Router>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -18,7 +21,7 @@ const Register = () => {
         e.preventDefault();
 
         if(password != password2) {
-            console.log('Passwords do not match.');
+            setAlert('Passwords do not match.', 'danger');
         } else {
             console.log('SUCCESS');
         }
@@ -65,7 +68,11 @@ const Register = () => {
                 Already have an account? <Link to='/login'>Sign In</Link>
             </p>
         </Fragment>
-    )
-}
+    );
+};
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
