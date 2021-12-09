b36b2f045729f0ce9404874ab9406364b2e368f6
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-12-09 00:37:02 -0600

    added create profile component and action

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -27,6 +27,13 @@ export default function(state = initialState, action) {
                 profile: null
             };
         
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
+                loading: false
+            }
         default:
             return state;
     }
